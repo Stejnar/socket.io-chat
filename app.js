d771c38756446b8ca73090cb05c614dfdd077002
Stejnar <a.butkereit@gmail.com>
2017-11-08 18:36:49 +0100

    private rooms and fetching users

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var user = [];
+var users = [];
 
 app.use(express.static(__dirname + '/src/public'));
 
@@ -11,24 +11,34 @@ app.get('/', function(req, res) {
 });
 
 io.on('connection', function(socket) {
+    var socket_room;
     console.log(socket.id);
     socket.on('new user', function(name) {
         if (name === 'Anonymous') {
-            name += user.length;
+            name += users.length;
         }
-        user.push({
+        users.push({
             socket: socket,
             username: name
         });
         socket.broadcast.emit('new user', name);
         console.log(name + ' joined the room');
     });
+    socket.on('fetch users', function(name) {
+        socket_room = name;
+        socket.join(socket_room);
+        console.log(name + ' is fetching users');
+        io.sockets.in(name).emit('fetch users', users);
+    });
     socket.on('disconnect', function() {
-        for (var i = 0; i < user.length; i++) {
-            if (socket === user[i].socket) {
-                io.emit('user left', user[i].username);
-                console.log(user[i].username + ' disconnected');
-                user.splice(i, 1);
+        if (socket_room) {
+            socket.leave(socket_room);
+        }
+        for (var i = 0; i < users.length; i++) {
+            if (socket === users[i].socket) {
+                io.emit('user left', users[i].username);
+                console.log(users[i].username + ' disconnected');
+                users.splice(i, 1);
             }
         }
     });
